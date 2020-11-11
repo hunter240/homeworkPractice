8b1a2cc4b32fdeb0b0b8cb3288d70ff9062d19b5
hunter240 <hunter.harris1@students.snow.edu>
2020-11-11 08:57:46 -0700

    added files

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using System.Net;
+
+namespace homeworkPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter a valid web address: ");
+            string url = ReadLine();
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                url = "https://world.episerver.com/cms/?q=pagetype";
+            }
+
+            var uri = new Uri(url);
+
+            WriteLine($"URL: {url}");
+            WriteLine($"Scheme: {uri.Scheme}");
+            WriteLine($"Port: {uri.Port}");
+            WriteLine($"Host: {uri.Host}");
+            WriteLine($"Path: {uri.AbsolutePath}");
+            WriteLine($"Query: {uri.Query}");
+
+            IPHostEntry entry = Dns.GetHostEntry(uri.Host);
+            
+            WriteLine($"{entry.HostName} has the following IP addresses:");
+            foreach (IPAddress address in entry.AddressList)
+            {
+                WriteLine($" {address}");
+            }
+        }
+    }
+}
