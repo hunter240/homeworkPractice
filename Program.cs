8f90e1e31c3f7265d57bb561aa8fccc47e12012f
hunter240 <hunter.harris1@students.snow.edu>
2020-11-13 01:37:26 -0700

    practiced code for the managing filesystems section.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using static System.Console;
-using System.Net;
+using static System.IO.Directory;
+using static System.Environment;
+using static System.IO.Path;
+//using System.Net;
 
 namespace homeworkPractice
 {
@@ -8,7 +11,10 @@ namespace homeworkPractice
     {
         static void Main(string[] args)
         {
-            WriteLine("Enter a valid web address: ");
+            //OutputFileSystemInfo();
+            WorkWithDrives();
+
+            /*WriteLine("Enter a valid web address: ");
             string url = ReadLine();
             if (string.IsNullOrWhiteSpace(url))
             {
@@ -30,6 +36,50 @@ namespace homeworkPractice
             foreach (IPAddress address in entry.AddressList)
             {
                 WriteLine($" {address}");
+            }
+            */
+
+        }
+        static void OutputFileSystemInfo()
+        {
+            WriteLine("{0,-33} {1}", "Path.PathSeparator", PathSeparator);
+            WriteLine("{0,-33} {1}", "Path.DirectorySeparatorChar",
+                DirectorySeparatorChar);
+            WriteLine("{0,-33} {1}", "Directory.GetCurrentDirectory()",
+                GetCurrentDirectory());
+            WriteLine("{0,-33} {1}", "Environment.CurrentDirectory",
+                CurrentDirectory);
+            WriteLine("{0,-33} {1}", "Environment.SystemDirectory",
+                SystemDirectory);
+            WriteLine("{0,-33} {1}", "Path.GetTempPath()", GetTempPath());
+
+            WriteLine("GetFolderPath(SpecialFolder");
+            WriteLine("{0,-33} {1}", " .System)",
+                GetFolderPath(SpecialFolder.System));
+            WriteLine("{0,-33} {1}", " .ApplicationData)",
+                GetFolderPath(SpecialFolder.ApplicationData));
+            WriteLine("{0,-33} {1}", " .MyDocuments)",
+                GetFolderPath(SpecialFolder.MyDocuments));
+            WriteLine("{0,-33} {1}", " .Personal)",
+                GetFolderPath(SpecialFolder.Personal));
+        }
+        static void WorkWithDrives()
+        {
+            WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18} | {4,18}",
+            "NAME", "TYPE", "FORMAT", "SIZE (BYTES)", "FREE SPACE");
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    WriteLine(
+                    "{0,-30} | {1,-10} | {2,-7} | {3,18:N0} | {4,18:N0}",
+                    drive.Name, drive.DriveType, drive.DriveFormat,
+                    drive.TotalSize, drive.AvailableFreeSpace);
+                }
+                else
+                {
+                    WriteLine("{0,-30} | {1,-10}", drive.Name, drive.DriveType);
+                }
             }
         }
     }
